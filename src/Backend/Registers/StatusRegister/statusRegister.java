df0650c04ad3f67b1182a51aff6b7a76a2760b87
Aiman <akanaan@stud.hs-offenburg.de>
2025-04-02 20:30:09 +0200

    updated status Register

--- src/Backend/Registers/StatusRegister/statusRegister.java
+++ src/Backend/Registers/StatusRegister/statusRegister.java
@@ -3,88 +3,90 @@ package Backend.Registers.StatusRegister;
 /**
  * The Zero flag is set when the result of an operation is 0.
  * The Carry flag is set when the result of an operation has a carry (overflow) in the most significant bit (MSB).
- * The Digit Carry (Halfcarry) flag is set when the result of an operation has a carry in the lower nibble (4 bits).
+ * The Digit Carry (Half carry) flag is set when the result of an operation has a carry in the lower nibble (4 bits).
+ * Bit0 = Carryflag,
+ * Bit1 = DC-Flag,
+ * Bit2 = Zero-Flag,
+ * Bit3 = Power-Down-Bit,
+ * Bit4 = Time-Out-Bit,
+ * Bit5 = RP0
+ * Bit6 = RP1 (Bei mehr als 2 Bänken notwendig)
+ * Bit7 = IRP (Bei mehr als 2 Bänken notwendig)
  */
 public class statusRegister {
-
-    /**
-     * Bit0 = Carryflag,
-     * Bit1 = DC-Flag,
-     * Bit2 = Zero-Flag,
-     * Bit3 = Power-Down-Bit,
-     * Bit4 = Time-Out-Bit,
-     * Bit5 = RP0
-     * Bit6 = RP1 (Bei mehr als 2 Bänken notwendig)
-     * Bit7 = IRP (Bei mehr als 2 Bänken notwendig)
-     **/
-
     // Flags
-    int[] flags = new int[8];
+    private final int[] flags;
+
+    public statusRegister() {
+        this.flags = new int[8];
+        for (int i = 0; i < flags.length; i++) {
+            flags[i] = 0;
+        }
+    }
 
     // Methoden (Getter und Setter)
-    public void setCarryFlag(int flag) {
-        flags[0] = flag;
+    public void setCarryFlag(boolean flag) {
+        flags[0] = flag ? 1 : 0;
     }
 
-    public void setDigitcarryFlag(int flag) {
-        flags[1] = flag;
-    } 
+    public void setDigitCarryFlag(boolean flag) {
+        flags[1] = flag ? 1 : 0;
+    }
     
-    public void setZeroFlag(int flag) {
-        flags[2] = flag;
+    public void setZeroFlag(boolean flag) {
+        flags[2] = flag ? 1 : 0;
     }
 
-    public void setPowerDownBit(int flag) {
-        flags[3] = flag;
+    public void setPowerDownBit(boolean flag) {
+        flags[3] = flag ? 1 : 0;
     }
 
-    public void setTimeOutBit(int flag) {
-        flags[4] = flag;
+    public void setTimeOutBit(boolean flag) {
+        flags[4] = flag ? 1 : 0;
     }
 
-    public void setRP0(int flag) {
-        flags[5] = flag;
+    public void setRP0(boolean flag) {
+        flags[5] = flag ? 1 : 0;
     }
 
-    public void setRP1(int flag) {
-        flags[6] = flag;
+    public void setRP1(boolean flag) {
+        flags[6] = flag ? 1 : 0;
     }
 
-    public void setIRP(int flag) {
-        flags[7] = flag;
-    }    
-
-    
-
-    public int getCarryFlag() {
-        return flags[0];
+    public void setIRP(boolean flag) {
+        flags[7] = flag ? 1 : 0;
     }
 
-    public int getDigitcarryFlag() {
-        return flags[1];
+
+    public boolean getCarryFlag() {
+        return flags[0] == 1;
     }
 
-    public int getZeroFlag() {
-        return flags[2];
+    public boolean getDigitcarryFlag() {
+        return flags[1] == 1;
     }
 
-    public int getPowerDownBit() {
-        return flags[3];
+    public boolean getZeroFlag() {
+        return flags[2] == 1;
     }
 
-    public int getTimeOutBit() {
-        return flags[4];
+    public boolean getPowerDownBit() {
+        return flags[3] == 1;
     }
 
-    public int getRP0() {
-        return flags[5];
+    public boolean getTimeOutBit() {
+        return flags[4] == 1;
     }
 
-    public int getRP1() {
-        return flags[6];
+    public boolean getRP0() {
+        return flags[5] == 1;
     }
 
-    public int getIRP() {
-        return flags[7];
+    public boolean getRP1() {
+        return flags[6] == 1;
+    }
+
+    public boolean getIRP() {
+        return flags[7] == 1;
     }
 }
