8ffcd1c2d0a1c50529bc5b05e93920fe17bf2d3e
MaggiFix <lukas.veit05@gmail.com>
2025-04-03 11:35:07 +0200

    moar updates to bytecommands

--- src/Backend/Befehle/Byteorientiert/byteCommands.java
+++ src/Backend/Befehle/Byteorientiert/byteCommands.java
@@ -9,29 +9,49 @@ public class byteCommands {
     /**
      * Adds working register and f
      * @param f
-     * @param wReg
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void ADDWF( int f, workingRegister wReg, int d, statusRegister status, dataMemory data) {
+    public static void ADDWF( int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
         int RP0 = status.getRP0() ? 1 : 0;
         int w = wReg.read();
-
         int result = w + data.readData(f, RP0);
+
+        status.setCarryFlag(result > 0xFF);
+        status.setDigitCarryFlag(((w & 0x0F) + (data.readData(f, RP0) & 0x0F)) > 0x0F);
+        status.setZeroFlag((result & 0xFF) == 0);
+
+        if(d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f, result, RP0);
+        }
 
     }
 
     /**
      * AND-Connection between Working register and f
      * @param f
-     * @param wReg
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void ANDWF(int f, workingRegister wReg, int d, statusRegister status, dataMemory data) {
+    public static void ANDWF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
         int RP0 = status.getRP0() ? 1 : 0;
         int w = wReg.read();
 
         int result = w & data.readData(f, RP0);
 
+        status.setZeroFlag((result & 0xFF) == 0);
+
+        if(d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f,result,RP0);
+        }
     }
 
     /**
@@ -54,22 +74,42 @@ public class byteCommands {
     /**
      * Forms complement of f
      * @param f
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void COMF(int f, int d, statusRegister status, dataMemory data) {
+    public static void COMF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
         int RP0 = status.getRP0() ? 1 : 0;
         int result = (~f)+1;
+
+        status.setZeroFlag((result & 0xFF) == 0);
+
+        if(d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f, result, RP0);
+        }
 
     }
 
     /**
      * Decrements f
      * @param f
-     * @param d Destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void DECF(int f, int d, statusRegister status, dataMemory data) {
+    public static void DECF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
         int RP0 = status.getRP0() ? 1 : 0;
-
+        int result = data.readData(f, RP0) - 1;
+        status.setZeroFlag((result & 0xFF) == 0);
+        if(d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f,result,RP0);
+        }
     }
 
     /**
@@ -84,10 +124,20 @@ public class byteCommands {
     /**
      * Increments f
      * @param f
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void INCF(int f, int d) {
-
+    public static void INCF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
+        int RP0 = status.getRP0() ? 1 : 0;
+        int result = data.readData(f, RP0) + 1;
+        status.setZeroFlag((result & 0xFF) == 0);
+        if(d == 0) {
+            wReg.write(result); 
+        } else {
+            data.writeData(f, result, RP0);
+        }
     }
 
     /**
@@ -102,11 +152,22 @@ public class byteCommands {
     /**
      * OR-connection between working register and f
      * @param f
-     * @param wReg
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void IORWF(int f, workingRegister wReg, int d) {
-        int w = wReg.read();
+    public static void IORWF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
+        int RP0 = status.getRP0() ? 1 : 0;
+        int result = wReg.read() | data.readData(f, RP0);
+
+        status.setZeroFlag((result & 0xFF) == 0);
+
+        if(d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f,result,RP0);
+        }
     }
 
     /**
@@ -116,7 +177,7 @@ public class byteCommands {
      */
     public static void MOVF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
         int RP0 = status.getRP0() ? 1 : 0;
-        wReg.write();
+        wReg.write(data.readData(f, RP0));
     }
 
     /**
@@ -124,8 +185,9 @@ public class byteCommands {
      * @param f
      * @param wReg
      */
-    public static void MOVWF(int f, workingRegister wReg) {
-        int w = wReg.read();
+    public static void MOVWF(int f, workingRegister wReg, statusRegister status, dataMemory data) {
+        int RP0 = status.getRP0() ? 1 : 0;
+        data.writeData(f, wReg.read(), RP0);
     }
 
     /**
@@ -140,7 +202,7 @@ public class byteCommands {
      * @param f
      * @param d destination
      */
-    public static void RLF(int f, int d) {
+    public static void RLF(int f, int d, statusRegister status, dataMemory data) {
 
     }
 
@@ -149,37 +211,73 @@ public class byteCommands {
      * @param f
      * @param d destination
      */
-    public static void RRF(int f, int d) {
+    public static void RRF(int f, int d, statusRegister status, dataMemory data) {
 
     }
 
     /**
      * Subtracts working register from content of f
      * @param f
-     * @param wReg
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void SUBWF(int f, workingRegister wReg, int d) {
+    public static void SUBWF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
+        int RP0 = status.getRP0() ? 1 : 0;
+
         int w = wReg.read();
+        int result = data.readData(f, RP0) - w;
+
+        status.setCarryFlag(result < 0);
+        status.setDigitCarryFlag(((w & 0x0F) - (data.readData(f, RP0) & 0x0F)) < 0);
+        status.setZeroFlag((result & 0xFF) == 0);
+
+        if(d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f,result,RP0);
+        }
     }
 
     /**
      * Swaps both halfbytes at f
      * @param f
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void SWAPF(int f, int d) {
-
+    public static void SWAPF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
+        int RP0 = status.getRP0() ? 1 : 0;
+        int leftWord = data.readData(f, RP0) & 0b1111<<4;
+        int rightWord = data.readData(f, RP0) & 0b00001111;
+        int result = leftWord >> 4 + rightWord << 4;
+        status.setZeroFlag((result & 0xFF) == 0);
+        if (d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f, result, RP0);
+        }
     }
 
     /**
      * EXCLUSIVE-OR of working register and f
      * @param f
-     * @param wReg
-     * @param d destination
+     * @param d destination-bit (0 = wReg, 1 = f)
+     * @param wReg working register
+     * @param status status register
+     * @param data data memory
      */
-    public static void XORWF(int f, workingRegister wReg, int d) {
-        int w = wReg.read();
+    public static void XORWF(int f, int d, workingRegister wReg, statusRegister status, dataMemory data) {
+        int RP0 = status.getRP0() ? 1 : 0;
+        int result = wReg.read() ^ data.readData(f, RP0);
+
+        if(d == 0) {
+            wReg.write(result);
+        } else {
+            data.writeData(f,result,RP0);
+        }
     }
 
 }
